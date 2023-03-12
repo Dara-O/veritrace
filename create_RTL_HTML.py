aeb18800df0d8f2dfe4d2d53a597053bfca36ab6
Dara-O <daraogunmola2@gmail.com>
2023-03-11 20:28:30 -0500

    added feature: RTL syntax highlighting

--- create_RTL_HTML.py
+++ create_RTL_HTML.py
@@ -3,6 +3,10 @@ import re
 import argparse
 import xml.etree.ElementTree as ET
 
+###################
+# FUNCTIONS
+###################
+
 def writeCSSFile(out_dir: str) -> str:
     """
     Responsibility
@@ -21,13 +25,31 @@ def writeCSSFile(out_dir: str) -> str:
 
     # some of the css below was adapted from: 
     # - https://stackoverflow.com/questions/41306797/html-how-to-add-line-numbers-to-a-source-code-block
+    # - https://highlightjs.org/
 
-    # FIXME: Consider reading the css file from a...
-    # ...template rather than hardcoding it
+    # WARNING: class names in css_text below must match class names used..
+    # ...in '_reRepl_replaceRTL' function
     css_text = \
 """
+.hljs-comment {
+  color: blue;
+}
+.hljs-keyword {
+    color: #a72a2c;
+    font-weight: bold;
+}
+.hljs-const {
+    color: #ff00ff;
+}
+.hljs-operator {
+    color: blue;
+}
 p {
     font-family: sans-serif;
+}
+
+pre {
+    font-size: 14px;
 }
 
 pre.code {
@@ -42,19 +64,20 @@ pre.code code {
 pre.code code:before {
     content: counter(listing) "| ";
     display: inline-block;
-    width: 8em;         
-    padding-left: auto; 
-    margin-left: auto;  
+    width: 5em;         
+    padding-left: none; 
+    margin-left: none;  
     text-align: right;  
+    color: gray
 }
 /* Section Animation */
 @import "compass/css3";
 
 :target {
-    -webkit-animation: target-fade 1.5s;
-    -moz-animation: target-fade 1.5s;
-    -o-animation: target-fade 1.5s;
-    animation: target-fade 1.5s;
+    -webkit-animation: target-fade 3600s;
+    -moz-animation: target-fade 3600s;
+    -o-animation: target-fade 3600s;
+    animation: target-fade 3600s;
 }
 
 @-webkit-keyframes target-fade {
@@ -134,6 +157,117 @@ def _HTML_createRTLTxt(rtl_text: str, parent_html_xe: ET.Element) -> ET.Element:
     
     return pre_xe
 
+def _reRepl_replaceRTL(match_obj: re.Match, init: bool=False) -> str:
+    """
+    Responsibility:
+
+    Wraps RTL code (given in ```match_obj```) with html...
+    ...tags to enable syntax highlighting when rendered
+
+    Parameters:
+
+    match_obj   :   re.Match object, passed from re.sub().
+
+    init        :   bool, denotes if invocation is meant to...
+                    ...initialize static vars
+    """
+
+    # to be used by code outside this function
+    _reRepl_replaceRTL.PATTERN = r"""(<code[^>]+>)([^<]*)(</code>)"""
+    
+    # to be used internally
+    _reRepl_replaceRTL.VERILOG_COMMENTS = "(//.*)"
+    _reRepl_replaceRTL.VERILOG_CONSTS = "(\\b[_A-Z0-9]+)"
+
+    _reRepl_replaceRTL.VERILOG_KEYWORDS = [
+        "accept_on", "alias", "always", "always_comb", "always_ff", "always_latch", "and", "assert", "assign", "assume", "automatic", "before", "begin", "bind", "bins", "binsof", "bit", "break", "bufif0", "bufif1", "byte", "case", "casex", "casez", "cell", "chandle", "checker", "class", "clocking", "cmos", "config", "const", "constraint", "context", "continue", "cover", "covergroup", "coverpoint", "cross", "deassign", "default", "defparam", "design", "disable", "dist", "do", "edge", "else", "end", "endcase", "endchecker", "endclass", "endclocking", "endconfig", "endfunction", "endgenerate", "endgroup", "endinterface", "endmodule", "endpackage", "endprimitive", "endprogram", "endproperty", "endspecify", "endsequence", "endtable", "endtask", "enum", "event", "eventually", "expect", "export", "extends", "extern", "final", "first_match", "for", "force", "foreach", "forever", "fork", "forkjoin", "function", "generate|5", "genvar", "global", "highz0", "highz1", "if", "iff", "ifnone", "ignore_bins", "illegal_bins", "implements", "implies", "import", "incdir", "include", "initial", "inout", "input", "inside", "instance", "int", "integer", "interconnect", "interface", "intersect", "join", "join_any", "join_none", "large", "let", "liblist", "library", "local", "localparam", "logic", "longint", "macromodule", "matches", "medium", "modport", "module", "nand", "negedge", "nettype", "new", "nexttime", "nmos", "nor", "noshowcancelled", "not", "notif0", "notif1", "or", "output", "package", "packed", "parameter", "pmos", "posedge", "primitive", "priority", "program", "property", "protected", "pull0", "pull1", "pulldown", "pullup", "pulsestyle_ondetect", "pulsestyle_onevent", "pure", "rand", "randc", "randcase", "randsequence", "rcmos", "real", "realtime", "ref", "reg", "reject_on", "release", "repeat", "restrict", "return", "rnmos", "rpmos", "rtran", "rtranif0", "rtranif1", "s_always", "s_eventually", "s_nexttime", "s_until", "s_until_with", "scalared", "sequence", "shortint", "shortreal", "showcancelled", "signed", "small", "soft", "solve", "specify", "specparam", "static", "string", "strong", "strong0", "strong1", "struct", "super", "supply0", "supply1", "sync_accept_on", "sync_reject_on", "table", "tagged", "task", "this", "throughout", "time", "timeprecision", "timeunit", "tran", "tranif0", "tranif1", "tri", "tri0", "tri1", "triand", "trior", "trireg", "type", "typedef", "union", "unique", "unique0", "unsigned", "until", "until_with", "untyped", "use", "uwire", "var", "vectored", "virtual", "void", "wait", "wait_order", "wand", "weak", "weak0", "weak1", "while", "wildcard", "wire", "with", "within", "wor", "xnor", "xor",
+        "\$finish", "\$stop", "\$exit", "\$fatal", "\$error", "\$warning", "\$info", "\$realtime", "\$time", "\$printtimescale", "\$bitstoreal", "\$bitstoshortreal", "\$itor", "\$signed", "\$cast", "\$bits", "\$stime", "\$timeformat", "\$realtobits", "\$shortrealtobits", "\$rtoi", "\$unsigned", "\$asserton", "\$assertkill", "\$assertpasson", "\$assertfailon", "\$assertnonvacuouson", "\$assertoff", "\$assertcontrol", "\$assertpassoff", "\$assertfailoff", "\$assertvacuousoff", "\$isunbounded", "\$sampled", "\$fell", "\$changed", "\$past_gclk", "\$fell_gclk", "\$changed_gclk", "\$rising_gclk", "\$steady_gclk", "\$coverage_control", "\$coverage_get", "\$coverage_save", "\$set_coverage_db_name", "\$rose", "\$stable", "\$past", "\$rose_gclk", "\$stable_gclk", "\$future_gclk", "\$falling_gclk", "\$changing_gclk", "\$display", "\$coverage_get_max", "\$coverage_merge", "\$get_coverage", "\$load_coverage_db", "\$typename", "\$unpacked_dimensions", "\$left", "\$low", "\$increment", "\$clog2", "\$ln", "\$log10", "\$exp", "\$sqrt", "\$pow", "\$floor", "\$ceil", "\$sin", "\$cos", "\$tan", "\$countbits", "\$onehot", "\$isunknown", "\$fatal", "\$warning", "\$dimensions", "\$right", "\$high", "\$size", "\$asin", "\$acos", "\$atan", "\$atan2", "\$hypot", "\$sinh", "\$cosh", "\$tanh", "\$asinh", "\$acosh", "\$atanh", "\$countones", "\$onehot0", "\$error", "\$info", "\$random", "\$dist_chi_square", "\$dist_erlang", "\$dist_exponential", "\$dist_normal", "\$dist_poisson", "\$dist_t", "\$dist_uniform", "\$q_initialize", "\$q_remove", "\$q_exam", "\$async\$and\$array", "\$async\$nand\$array", "\$async\$or\$array", "\$async\$nor\$array", "\$sync\$and\$array", "\$sync\$nand\$array", "\$sync\$or\$array", "\$sync\$nor\$array", "\$q_add", "\$q_full", "\$psprintf", "\$async\$and\$plane", "\$async\$nand\$plane", "\$async\$or\$plane", "\$async\$nor\$plane", "\$sync\$and\$plane", "\$sync\$nand\$plane", "\$sync\$or\$plane", "\$sync\$nor\$plane", "\$system", "\$display", "\$displayb", "\$displayh", "\$displayo", "\$strobe", "\$strobeb", "\$strobeh", "\$strobeo", "\$write", "\$readmemb", "\$readmemh", "\$writememh", "\$value\$plusargs", "\$dumpvars", "\$dumpon", "\$dumplimit", "\$dumpports", "\$dumpportson", "\$dumpportslimit", "\$writeb", "\$writeh", "\$writeo", "\$monitor", "\$monitorb", "\$monitorh", "\$monitoro", "\$writememb", "\$dumpfile", "\$dumpoff", "\$dumpall", "\$dumpflush", "\$dumpportsoff", "\$dumpportsall", "\$dumpportsflush", "\$fclose", "\$fdisplay", "\$fdisplayb", "\$fdisplayh", "\$fdisplayo", "\$fstrobe", "\$fstrobeb", "\$fstrobeh", "\$fstrobeo", "\$swrite", "\$swriteb", "\$swriteh", "\$swriteo", "\$fscanf", "\$fread", "\$fseek", "\$fflush", "\$feof", "\$fopen", "\$fwrite", "\$fwriteb", "\$fwriteh", "\$fwriteo", "\$fmonitor", "\$fmonitorb", "\$fmonitorh", "\$fmonitoro", "\$sformat", "\$sformatf", "\$fgetc", "\$ungetc", "\$fgets", "\$sscanf", "\$rewind", "\$ftell", "\$ferror"
+        ]
+    _reRepl_replaceRTL.VERILOG_KEYWORDS = "\\b|\\b".join(_reRepl_replaceRTL.VERILOG_KEYWORDS)
+    _reRepl_replaceRTL.VERILOG_KEYWORDS =  "(\\b"+_reRepl_replaceRTL.VERILOG_KEYWORDS + "\\b)"
+
+    _reRepl_replaceRTL.VERILOG_OPERATORS = [
+        "\(", "\)", "\|", "\[", "\]", "{", "}", "&lt;", "&gt;", "-", "&amp;", "=", "==", "===", "!=", "!==", ",", ";", ":", "/"
+        ]
+    _reRepl_replaceRTL.VERILOG_OPERATORS = "|".join(_reRepl_replaceRTL.VERILOG_OPERATORS)
+    _reRepl_replaceRTL.VERILOG_OPERATORS = "(\\b" + _reRepl_replaceRTL.VERILOG_OPERATORS + "\\b)"
+
+
+    # WARNING: The class names below must match class names in...
+    #           ...the CSS file (see function: 'writeCSSFile')
+    KEYWORD_REPL    = '<span class="hljs-keyword">\\1</span>'
+    OPERATOR_REPL   = '<span class="hljs-operator">\\1</span>'
+    COMMENT_REPL    = '<span class="hljs-comment">\\1</span>'
+    CONST_REPL      = '<span class="hljs-const">\\1</span>'
+
+    # replace certain words and symbols to prevent...
+    # ...complicts between substitutions
+    conflict_avoidance_repl = [("class=", "<relpace_me_w_plass_eq>"),
+                               ("-", "<relpace_me_w_minus>")]
+
+    for orig, repl in conflict_avoidance_repl:
+        KEYWORD_REPL = KEYWORD_REPL.replace(orig, repl)
+        OPERATOR_REPL = OPERATOR_REPL.replace(orig, repl)
+        COMMENT_REPL = COMMENT_REPL.replace(orig, repl)
+        CONST_REPL = CONST_REPL.replace(orig, repl)
+
+
+    if(not init):
+        rtl_txt_combined = match_obj.group(2)
+
+        if("//" in rtl_txt_combined):
+            rtl_txt, rtl_comment = tuple(rtl_txt_combined.split("//", maxsplit=1))
+        else:
+            rtl_txt = rtl_txt_combined
+            rtl_comment = ""
+
+        if(rtl_comment.strip() != ""):
+            rtl_comment = "//"+ rtl_comment
+
+        # format comments
+        rtl_comment = re.sub(_reRepl_replaceRTL.VERILOG_COMMENTS, COMMENT_REPL, rtl_comment)
+
+        # format operators
+        rtl_txt = re.sub(_reRepl_replaceRTL.VERILOG_OPERATORS, OPERATOR_REPL, rtl_txt)
+
+        # format keywords
+        rtl_txt = re.sub(_reRepl_replaceRTL.VERILOG_KEYWORDS, KEYWORD_REPL, rtl_txt)
+
+        # format const
+        rtl_txt = re.sub(_reRepl_replaceRTL.VERILOG_CONSTS, CONST_REPL, rtl_txt)
+        
+        rtl_txt_combined = rtl_txt + rtl_comment
+        rtl_txt_combined = rtl_txt_combined.replace("<relpace_me_w_plass_eq>", "class=")
+        rtl_txt_combined = rtl_txt_combined.replace("<relpace_me_w_minus>", "-")
+        return (match_obj.group(1)+rtl_txt_combined+match_obj.group(3))
+
+# initialize static vars
+_reRepl_replaceRTL(None, init=True)
+
+def format_RTLtxt(html_str: str) -> str:
+    """
+    Responsibility:
+
+    Format RTL code within the html text to enable syntax highlighting
+
+    Returns:
+
+    string, containing formatted RTL code
+
+    Parameters:
+
+    html_str    :   string, contains html text
+    """
+
+    # replace all '&' with a placeholder text
+    # this is done to prevent the javascript function from adversely modifying the html
+
+    # regex replace
+    out_str = re.sub(_reRepl_replaceRTL.PATTERN, _reRepl_replaceRTL, html_str)
+    # replace AMP_PLACEHOLDER
+    
+    return out_str
+
 def writeRTL_HTML(rtl_file_path: str, html_file_path: str, css_file_path: str):
     """
     Responsibility
@@ -177,19 +311,27 @@ def writeRTL_HTML(rtl_file_path: str, html_file_path: str, css_file_path: str):
     # write html file
     html_xe = ET.ElementTree(html_xe)
     ET.indent(html_xe)
-    html_xe.write(html_file_path, method='html', short_empty_elements=False)
+    ET.indent(rtl_xe, space="")
+
+    # format rtl to create syntax higlighting
+    html_str = ET.tostring(
+        html_xe.getroot(), 
+        method='html', 
+        short_empty_elements=False
+    )
+    html_str = html_str.decode("ascii")
+
+    html_str = format_RTLtxt(html_str)
+
+    # html_xe.write(html_file_path, method='html', short_empty_elements=False)
+    with open(html_file_path, "w") as f:
+        f.write(html_str)
 
     return html_file_path
 
-    # write the name of the RTL file as a header...
-    # Eg:
-    #   **Filename**: instruction_cache.v
-
-    # Next, RTL code listing:
-    # Inspiration
-    # - https://stackoverflow.com/questions/41306797/html-how-to-add-line-numbers-to-a-source-code-block
-    # - https://www.hdlworks.com/products/companion/eth_html/fuz_a0a0a070bc038d162af15600c378a62f.htm#Line_75 
-
+###################
+# MAIN
+###################
 
 parser = argparse.ArgumentParser(description="Create a series of HTML pages containing RTL code")
 
@@ -208,6 +350,6 @@ for rtl_file in args.rtl_files:
     rtl_file_name = os.path.basename(rtl_file).split(".")[:-1]
     rtl_file_name = ".".join(rtl_file_name)
 
-    html_file_path = os.path.join(args.out_dir, rtl_file_name) + ".html"
+    html_file_path = os.path.join(args.out_dir, rtl_file_name) + ".v.html"
 
     writeRTL_HTML(rtl_file, html_file_path, css_file_path)
